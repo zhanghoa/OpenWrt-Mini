8734e39ea5a206280ac25e982153ef8107beb17c
zhanghoa <42800990+zhanghoa@users.noreply.github.com>
2024-04-15 16:13:05 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -17,3 +17,8 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 5-添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 6-添加istore插件
+echo "src-git istore https://github.com/linkease/istore" >> "feeds.conf.default"
+echo "src-git nas https://github.com/linkease/nas-packages.git" >> "feeds.conf.default"
+echo "src-git nas_luci https://github.com/linkease/nas-packages-luci.git" >> "feeds.conf.default"
