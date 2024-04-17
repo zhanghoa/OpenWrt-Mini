474560c4c2504eb5d4ea8046bfbeefc7f5f10ac1
zhanghoa <42800990+zhanghoa@users.noreply.github.com>
2024-04-17 11:02:14 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -19,6 +19,4 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
 # 6-添加istore插件
-echo "src-git istore https://github.com/linkease/istore" >> "feeds.conf.default"
-echo "src-git nas https://github.com/linkease/nas-packages.git" >> "feeds.conf.default"
-echo "src-git nas_luci https://github.com/linkease/nas-packages-luci.git" >> "feeds.conf.default"
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
